41804b857f4fc9148a2e7c66d89edc976f2f9422
Sennay <wsennay@gmail.com>
2024-03-10 12:51:36 +0300

    feat/setup validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      stopAtFirstError: true,
+    }),
+  );
+
+  await app.listen(4000);
 }
 bootstrap();
