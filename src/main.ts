cacbeccce639f6387567ff4c76b842e188a0dee0
Sennay <wsennay@gmail.com>
2024-03-12 11:57:25 +0300

    feat/setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API Documentation')
+    .addBearerAuth()
+    .addCookieAuth('x-refresh-token')
+    .setVersion('v1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {});
+  SwaggerModule.setup('explorer', app, document, {
+    swaggerOptions: {
+      docExpansion: 'none',
+      plugins: [
+        () => {
+          return {
+            wrapComponents: {
+              curl: () => () => null,
+            },
+          };
+        },
+      ],
+    },
+  });
 
   app.enableCors({
     allowedHeaders: ['Content-Type', 'Authorization', 'X-refresh-token'],
